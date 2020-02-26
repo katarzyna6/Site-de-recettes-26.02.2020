22dc73a4059bf7cb783a3a63cd232b99611e262a
katarzyna <katharina.r@hotmail.fr>
2020-02-26 11:29:31 +0100

    Ajout fonctionnalité modale html + css + js

--- script_entrees.js
+++ script_entrees.js
@@ -2,8 +2,16 @@
 console.log('OK');
 
 var recettes = [
-    {titre: "Salade Niçoise", page: "recette_salade_nicoise.html"},
-    {titre: "Tarte aux poireaux", page: "pages_entrees/recette_tarte_poireaux.html"}
+    {
+        titre: "Salade Niçoise", 
+        page: "recette_salade_nicoise.html",
+        image: "salade.jpg"
+    },
+    {
+        titre: "Tarte aux poireaux", 
+        page: "pages_entrees/recette_tarte_poireaux.html",
+        image: "tarte.jpg"
+    }
 ];
 
 var html = "";
@@ -19,9 +27,33 @@ for(var i = 0; i< len; i++) {
     //<li><a href="pages_entrees/recette_tarte_poireaux.html">Tarte aux poireaux</a></li>
     //====> console.log("<li><a href=\"pages_entrees/" + recettes[i].page + "\">" + recettes[i].titre + "</li>");
 
-    html = html + "<li><a href=\"pages_entrees/" + recettes[i].page + "\">" + recettes[i].titre + "</li>"
-}
+    html = html + "<li class=\"elem\" id=\"rec" + i + "\">" + recettes[i].titre + "</li>"
+};
 
 console.log(html);
 var liste = document.getElementById("liste");
 liste.innerHTML = html;
+
+/* Fonctionnalité modale */ 
+
+var container = document.getElementById("rec_container");
+container.addEventListener("click", () =>{
+    container.style.display = "none";
+});
+
+var elems = document.getElementsByClassName("elem");
+var len = elems.length;
+for(var i = 0; i < len; i++) {
+    elems[i].addEventListener("click", (event) => {
+        event.preventDefault();
+        var index = parseInt(event.target.id.substring(3));
+        showRecette(index);
+    });
+}
+console.log(elems);
+
+function showRecette(index) {
+    console.log("Vous avez demandé à la recette n°" + index)
+    container.style.display = "block";
+
+}
